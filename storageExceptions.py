570e25b9b17bc58dbcdf8a28ed3022f1ca0b8e7c
MMogna <mmogna@elemento.cloud>
2024-12-04 10:43:11 +0100

    new storage endpoints and exceptions

--- storageExceptions.py
+++ storageExceptions.py
@@ -45,8 +45,26 @@ class VolumeDataDoesNotExist(Exception):
     def __str__(self):
         return f"[{type(self).__name__}] {self.message}"
 
-
+### EXPORT ###
 class VolumeAlreadyExported(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+    
+
+class VolumeNotExportable(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+
+### METADATA NOT VALID ###
+class VolumeBusNotAllowed(Exception):
     def __init__(self, message):
         super().__init__(message)
         self.message = message
@@ -55,6 +73,25 @@ class VolumeAlreadyExported(Exception):
         return f"[{type(self).__name__}] {self.message}"
 
 
+class VolumeFormatNotAllowed(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+
+
+class VolumeCloningAlgNotAllowed(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+
+
+### METADATA UPDATE ###
 class VolumeMetadataNotUpdatable(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -80,8 +117,18 @@ class VolumeImpossibleCreatorIDMetaUpdate(Exception):
 
     def __str__(self):
         return f"[{type(self).__name__}] {self.message}"
+    
+
+class VolumeImpossibleCloudinitMetaUpdate(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
 
 
+### RESIZE ###
 class VolumeDimensionNotUpdatable(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -98,17 +145,9 @@ class VolumeDimensionNotShrinkable(Exception):
 
     def __str__(self):
         return f"[{type(self).__name__}] {self.message}"
-
-
-class VolumeNotExportable(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
-
-    def __str__(self):
-        return f"[{type(self).__name__}] {self.message}"
     
 
+### CREATE ###
 class VolumeNotAllocable(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -145,6 +184,7 @@ class VolumeFormatNotFound(Exception):
         return f"[{type(self).__name__}] {self.message}"
     
 
+### LOCK ACTIONS ###
 class VolumeAlredyLocked(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -152,14 +192,7 @@ class VolumeAlredyLocked(Exception):
 
     def __str__(self):
         return f"[{type(self).__name__}] {self.message}"
-    
-class VolumeCannotBeDeleted(Exception):
-    def __init__(self, message):
-        super().__init__(message)
-        self.message = message
 
-    def __str__(self):
-        return f"[{type(self).__name__}] {self.message}"
 
 class VolumeAlredyUnlocked(Exception):
     def __init__(self, message):
@@ -168,16 +201,18 @@ class VolumeAlredyUnlocked(Exception):
 
     def __str__(self):
         return f"[{type(self).__name__}] {self.message}"
-    
 
-class CloudInitDownloadfailed(Exception):
+
+### BIN AND DELETE ###
+class VolumeCannotBeDeleted(Exception):
     def __init__(self, message):
         super().__init__(message)
         self.message = message
 
     def __str__(self):
         return f"[{type(self).__name__}] {self.message}"
-    
+
+
 class ElementoBinNotFound(Exception):
     def __init__(self, message):
         super().__init__(message)
@@ -185,3 +220,70 @@ class ElementoBinNotFound(Exception):
 
     def __str__(self):
         return f"[{type(self).__name__}] {self.message}"
+
+
+### CLOUDINIT ###
+class CloudInitCreateImageDownloadfailed(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+
+
+class CloudInitMetadataUploadFailed(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+
+
+class CloudInitMetadataFileNotFound(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+
+
+class CloudInitIsoCreationFailed(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+
+
+class ClouInitIsoNotFound(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+
+
+class CloudInitMetafilesNumberExceeded(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+
+
+class CloudInitExpectedFilesTrackerNotFound(Exception):
+    def __init__(self, message):
+        super().__init__(message)
+        self.message = message
+
+    def __str__(self):
+        return f"[{type(self).__name__}] {self.message}"
+    
+
+# class 
